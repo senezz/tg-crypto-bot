b92788d170a7d234caa18df6bb46add1ce80a1f6
senezz <senezzyx@gmail.com>
2026-03-16 21:47:52 +0100

    added generating code for verification

--- main.js
+++ main.js
@@ -3,14 +3,26 @@ DotEnv.config();
 const TelegramBot = require("node-telegram-bot-api");
 console.log("start");
 const bot = new TelegramBot(process.env.BOT_API_TOKEN, { polling: true });
+
+function generateCodeVerification() {
+  return Math.floor(Math.random() * 900_000 + 100_000);
+}
+
+bot.onText(/\/start/, (input) => {
+  const chatId = input.chat.id;
+  const userId = input.from.id;
+  const username = input.from.username;
+  const code = generateCodeVerification();
+  bot.sendMessage(chatId, `Your Verification Code is: \`${code}\``, {
+    parse_mode: "MarkdownV2",
+  });
+  // console.log({ chatId, username, userId });
+});
 bot.on("message", (message) => {
   const chatId = message.chat.id;
   const textMessage = message.text;
-  console.log({ chatId, textMessage });
+  // console.log({ chatId, textMessage, message });
   bot.sendMessage(chatId, "Hello Sir Dear BOSS");
 });
-/* 
-
-*/
 
 setInterval(() => {}, 1000);
