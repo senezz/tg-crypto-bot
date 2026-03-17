3c771d6bab871c71ca0a1917fa518c346e2ab5af
senezz <senezzyx@gmail.com>
2026-03-17 21:54:39 +0100

    deployed bot

---
+++ functions/index.js
@@ -0,0 +1,78 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+const { setGlobalOptions } = require("firebase-functions");
+const { onRequest } = require("firebase-functions/https");
+const logger = require("firebase-functions/logger");
+const { initializeApp } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
+
+// For cost control, you can set the maximum number of containers that can be
+// running at the same time. This helps mitigate the impact of unexpected
+// traffic spikes by instead downgrading performance. This limit is a
+// per-function limit. You can override the limit for each function using the
+// `maxInstances` option in the function's options, e.g.
+// `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
+// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1
+// functions should each use functions.runWith({ maxInstances: 10 }) instead.
+// In the v1 API, each function can only serve one request per container, so
+// this will be the maximum concurrent request count.
+setGlobalOptions({ maxInstances: 10 });
+
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+initializeApp();
+
+const DotEnv = require("dotenv");
+DotEnv.config();
+const TelegramBot = require("node-telegram-bot-api");
+console.log("start");
+const bot = new TelegramBot(process.env.BOT_API_TOKEN);
+
+exports.tgBot = onRequest(async (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
+
+function generateCodeVerification() {
+  return Math.floor(Math.random() * 900_000 + 100_000);
+}
+
+bot.onText(/\/start/, async (input) => {
+  const chatId = input.chat.id;
+  const userId = input.from.id;
+  const username = input.from.username;
+  const code = generateCodeVerification();
+
+  console.log(`code was generated: ${code}`);
+
+  await getFirestore().collection("tg-codes").doc(chatId.toString()).set({
+    code,
+    userId,
+    username,
+    timestamp: new Date(),
+  });
+
+  bot.sendMessage(chatId, `Your Verification Code is: \`${code}\``, {
+    parse_mode: "MarkdownV2",
+  });
+  // console.log({ chatId, username, userId });
+});
+bot.on("message", (message) => {
+  const chatId = message.chat.id;
+  const textMessage = message.text;
+  // console.log({ chatId, textMessage, message });
+  bot.sendMessage(chatId, "Hello Sir Dear BOSS");
+});
--- main.js
+++ main.js
@@ -1,8 +1,16 @@
+const functions = require("firebase-functions");
+
 const DotEnv = require("dotenv");
 DotEnv.config();
 const TelegramBot = require("node-telegram-bot-api");
+const { onRequest } = require("firebase-functions/https");
 console.log("start");
-const bot = new TelegramBot(process.env.BOT_API_TOKEN, { polling: true });
+const bot = new TelegramBot(process.env.BOT_API_TOKEN);
+
+exports.tgBot = onRequest(async (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
 
 function generateCodeVerification() {
   return Math.floor(Math.random() * 900_000 + 100_000);
